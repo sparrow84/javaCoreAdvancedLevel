0c0f946f37828ba103aaec2226b14839cae463b1
sparrow84 <vorobey280184@mail.ru>
2018-08-26 20:36:15 +0700

    Закончил методы

--- src/main/java/com/company/lesson02/Main.java
+++ src/main/java/com/company/lesson02/Main.java
@@ -31,7 +31,7 @@ public class Main {
         };
 
         String[][] arr4 = {
-                {"++","02","03","04"},
+                {"01","02","03","04"},
                 {"05","06","07","08"},
                 {"09","10","11","12"},
                 {"13","14","15","16"}
@@ -42,7 +42,7 @@ public class Main {
 //        print2dArray(arr);
 
         try {
-            sumIntArray4by4(arr);
+            sumIntArray4by4(arr4);
         } catch (MyArraySizeException e) {}
         catch (MyArrayDataException e) {}
 
