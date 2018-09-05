f23059d1ad820b687055157ccb0d4bb5ef5be585
sparrow84 <vorobey280184@mail.ru>
2018-09-05 22:03:42 +0700

    Закончил lesson05

--- src/main/java/com/company/lesson05/Main.java
+++ src/main/java/com/company/lesson05/Main.java
@@ -6,15 +6,14 @@ public class Main {
     static final int h = size / 2;
 
     public static void main(String[] args) {
-//        System.out.println("firstM - " + firstM());
+        System.out.println("firstM - " + firstM());
         System.out.println("secondM - " + secondM());
-        System.out.println("Главный поток завершён...");
+//        System.out.println("Главный поток завершён...");
     }
 
     public static long firstM () {
 
         long tStart;
-        long tDelta;
         float[] arr = new float[size];
 
         for (int i = 0; i < size; i++) {
@@ -47,31 +46,24 @@ public class Main {
         System.arraycopy(arr, h, a2, 0, h);
 
         new Thread(() -> {
-            System.out.println("Привет из потока " + Thread.currentThread().getName());
+//            System.out.println("Привет из потока " + Thread.currentThread().getName());
             calcArray(a1);
         }).start();
 
         new Thread(() -> {
-            System.out.println("Привет из потока " + Thread.currentThread().getName());
+//            System.out.println("Привет из потока " + Thread.currentThread().getName());
             calcArray(a2);
         }).start();
-
-//        myT1.start(); // Thread myT1 =
-//        myT2.start(); // Thread myT2 =
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
 
-        Thread myT3 = new Thread(() -> {
-            System.out.println("Привет из потока 3!");
-        });
+//        Thread myT3 = new Thread(() -> {
+//            System.out.println("Привет из потока 3!");
+//        });
+//        myT3.start();
 
-        try {
-            myT3.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
+//        System.out.println("Перед ретёрном.");
         return System.currentTimeMillis() - tStart;
     }
 
